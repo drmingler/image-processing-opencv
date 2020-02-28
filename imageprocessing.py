a47d16e92a358c98254896ac868248e14e5e8710
Zaddy6 <isaac.david57@gmail.com>
2020-02-28 16:29:44 +0100

    Error handling

--- imageprocessing.py
+++ imageprocessing.py
@@ -39,29 +39,32 @@ def index():
 
 @app.route('/multiple', methods=['GET', 'POST'])
 def multiple():
-    if request.method == "POST":
-        for image in request.files.getlist('file'):
-            image.save(os.path.join(os.path.abspath('imageProcessing'), image.filename))
-            print(image)
-        files=request.files.getlist('file')
-        print(files[0].filename)
-        print(files[1].filename)
+    try:
+        if request.method == "POST":
+            for image in request.files.getlist('file'):
+                image.save(os.path.join(os.path.abspath('imageProcessing'), image.filename))
+                print(image)
+            files=request.files.getlist('file')
+            print(files[0].filename)
+            print(files[1].filename)
 
-        image1=os.path.join(os.path.abspath('imageProcessing'), files[0].filename)
-        image2=os.path.join(os.path.abspath('imageProcessing'), files[1].filename)
-        # image1=str(files[0].filename)
-        # image2 = str(files[1].filename)
+            image1=os.path.join(os.path.abspath('imageProcessing'), files[0].filename)
+            image2=os.path.join(os.path.abspath('imageProcessing'), files[1].filename)
+            # image1=str(files[0].filename)
+            # image2 = str(files[1].filename)
 
-        image = stich(image1, image2)
+            image = stich(image1, image2)
 
-        concatstr = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(5))
-        imagename = "Panorama" + concatstr + '.jpg'
-        cv2.imwrite(f'static/{imagename}', image)
-        Panorama = os.path.join(('static'), imagename)
-        return render_template("results.html", image=Panorama)
+            concatstr = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(5))
+            imagename = "Panorama" + concatstr + '.jpg'
+            cv2.imwrite(f'static/{imagename}', image)
+            Panorama = os.path.join(('static'), imagename)
+            return render_template("results.html", image=Panorama)
+    except:
+        return('''<h1>Invalid Image Format</h1''')
 
-
-    return render_template("multiple.html")
+    else:
+        return render_template("multiple.html")
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
